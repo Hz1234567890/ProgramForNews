89b2aa8e91c01ff7f0bfa2c2c6a4ddcb5f3e1be9
hz <2144654256@qq.com>
2023-10-06 08:31:05 +0800

    first

---
+++ header.h
@@ -0,0 +1,8 @@
+//header.h
+#ifndef HEADER_H
+#define HEADER_H
+
+//函数声明
+void print();
+
+#endif
---
+++ scoure.cpp
@@ -0,0 +1,14 @@
+//scoure.cpp
+
+#include"header.h"
+#include<iostream>
+
+//函数定义
+void print(){
+    printf("HelloWorld!");
+}
+
+int main(){
+    print();
+    return 0;
+}
